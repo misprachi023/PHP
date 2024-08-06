787165246ad95d76c303cc534e019f45b8b47bca
misprachi023 <prachimishra23123@gmail.com>
2024-08-06 22:31:56 +0530

    add some basic code

---
+++ Learn-PHP/basic-function.php
@@ -0,0 +1,23 @@
+<?php
+function add(){
+    $a=10;
+    $b=14;
+    echo $a+$b;
+}
+add();
+?>
+
+<?php
+function details(){
+    add();
+    echo "My name is Prachi Mishra";
+    echo "<br/>";
+    echo "I am 20 years old";
+    echo "<br/>";
+    echo "I live in Bhopal";
+    echo "<br/>";
+    echo "<hr/>";
+}
+details();
+details();
+?>
